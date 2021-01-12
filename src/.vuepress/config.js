88e266363af2fc89f2af56a9a9ffe650001fa5dd
SeanChao <seanchao0804@gmail.com>
2021-01-12 20:47:56 +0800

    feat: refactor with vuepress

---
+++ src/.vuepress/config.js
@@ -0,0 +1,70 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Hi, CS 👋',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#0277bd' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: 'https://github.com/xingzhi-automata/HiCS',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: true,
+    nav: [
+      {
+        text: '介绍',
+        link: '/intro/',
+        items: [
+          {text: '前言', link: '/intro/'},
+          {text: '简介', link: '/intro/welcome'}
+        ]
+      }
+    ],
+    sidebar: {
+      '/intro/': [
+        '',
+        'welcome',
+      ],
+    }
+  },
+  markdown: {
+    toc: {
+      includeLevel: [2, 3]
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ],
+  locales: {
+    '/': {
+      lang: 'zh-CN'
+    }
+  }
+}
---
+++ src/index.md
@@ -0,0 +1,17 @@
+---
+home: true
+heroImage: /icon.png
+tagline: 给行知人的 CS 指北
+actionText: GoGoGo →
+actionLink: /intro/
+features:
+- title: Artificial Intelligence
+  details: 🤖人工智能
+- title: Computer Science
+  details: 🧮 计算机科学与技术
+- title: Software Enginering
+  details: 👩‍💻 软件工程
+- title: Information Security
+  details: 🔒 信息/网络空间安全
+footer: Made by Xingzhi-automata with ❤️
+---
